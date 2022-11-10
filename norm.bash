e2c4e2fb00a83004aef944f69a997e5cb3cf64a7
mchenava <matisse.chenavas@gmail.com>
2022-11-10 11:42:22 +0100

    update makefile

--- norm.bash
+++ norm.bash
@@ -1,9 +1,8 @@
-files = $(find . -type f -name "*.c" -o -name "*.h")
-
 if norminette; then 
 	echo "norminette passed" 
 else 
-	for file in $files; do 
+	for file in $(find . -type f -name "*.c" -o -name "*.h"); do
+		echo "norminette failed on $file - norming $file"
 		c_formatter_42 $file 
 		norminette $file 
 	done 
