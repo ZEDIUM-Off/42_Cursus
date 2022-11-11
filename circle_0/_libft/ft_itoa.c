542e842d6a84fee3943dada08d6066259fc30c11
mchenava <matisse.chenavas@gmail.com>
2022-11-11 09:33:49 +0100

    send  to github

--- circle_0/_libft/ft_itoa.c
+++ circle_0/_libft/ft_itoa.c
@@ -6,11 +6,26 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:39:05 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/10 17:44:35 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/11 09:33:24 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_itoa(int n)
 {
-	
+	char	*str;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n < 0)
+		return (ft_strjoin("-", ft_itoa(-n)));
+	if (n < 10)
+	{
+		str = (char *)malloc(sizeof(char) * 2);
+		if (!str)
+			return (NULL);
+		str[0] = n + '0';
+		str[1] = '\0';
+		return (str);
+	}
+	return (ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10)));
 }
--- circle_0/_libft/ft_strtrim.c
+++ circle_0/_libft/ft_strtrim.c
@@ -6,11 +6,22 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:14:19 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/10 17:18:35 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/11 09:31:53 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	j = ft_strlen(s1) - 1;
+	while (j > i && ft_strchr(set, s1[j]))
+		j--;
+	return (ft_substr(s1, i, ft_strlen(s1) - j + 1));
 }
