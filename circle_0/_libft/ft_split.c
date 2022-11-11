b6159586060cc1498526683e4de29605ddc2771d
mchenava <matisse.chenavas@gmail.com>
2022-11-11 09:35:33 +0100

    send  to github

--- circle_0/_libft/ft_split.c
+++ circle_0/_libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:23:08 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/10 17:39:24 by marvin           ###   ########.fr       */
+/*   Updated: 2022/11/11 09:35:27 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@ char	**ft_split(char const *s, char c)
 	int		j;
 	int		k;
 
-	tab = (char **)malloc(sizeof(char *) * (ft_count_words(str, c) + 1));
+	tab = (char **)malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
 	i = 0;
 	k = 0;
-	while (src[i])
+	while (s[i])
 	{
-		while (src[i] && src[i] == c)
+		while (s[i] && s[i] == c)
 			i++;
 		j = i;
-		while (src[j] && src[j] != c)
+		while (s[j] && s[j] != c)
 			j++;
 		tab[k] = (char *)malloc(sizeof(char) * (j - i + 1));
 		j = 0;
-		while (src[i] && src[i] != c)
-			tab[k][j++] = src[i++];
+		while (s[i] && s[i] != c)
+			tab[k][j++] = s[i++];
 		tab[k++][j] = '\0';
 	}
 	tab[k] = 0;
