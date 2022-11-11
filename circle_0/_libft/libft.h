2b9a08e94c1da31cbc775c8be7db299a492bc34e
mchenava <matisse.chenavas@gmail.com>
2022-11-11 09:49:53 +0100

    send  to github

--- circle_0/_libft/libft.h
+++ circle_0/_libft/libft.h
@@ -44,8 +44,8 @@ size_t		ft_strlen(const char *s);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 
-void		*ft_memset (void *s, int c, size_t n);
-void		*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memcpy(void* restrict dest, const void* restrict src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
 void		*ft_calloc(size_t nmemb, size_t size);
