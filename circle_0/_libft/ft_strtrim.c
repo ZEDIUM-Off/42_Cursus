cf8396454bb2bea0904136d78aac7060ebc82283
Chenavas Matisse <matisse.chenavas@gmail.com>
2022-11-14 11:44:21 +0100

    send 	modified:   circle_0/_libft/ft_strtrim.c to github

--- circle_0/_libft/ft_strtrim.c
+++ circle_0/_libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By:  mchenava < mchenava@student.42lyon.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:14:19 by mchenava          #+#    #+#             */
-/*   Updated: 2022/11/14 11:32:04 by  mchenava        ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 11:44:18 by  mchenava        ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ char	*ft_strtrim(char const *s1, char const *set)
 	j = ft_strlen(s1) - 1;
 	while (j > i && ft_strchr(set, s1[j]))
 		j--;
-	return (ft_substr(s1, i, ft_strlen(s1) - j));
+	return (ft_substr(s1, i, j - i + 1));
 }
