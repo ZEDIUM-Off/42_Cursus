b5b27eb8dbf79063aa53a6536d9e4ed33b9f2d2d
Chenavas Matisse <matisse.chenavas@gmail.com>
2022-11-25 17:55:53 +0100

    send On branch circle_1 Your branch is up to date with origin/circle_1. 	modified:   circle_1/_get_next_line/get_next_line.c 	circle_1/_get_next_line/a.out.dSYM/Contents/Info.plist 	circle_1/_get_next_line/a.out.dSYM/Contents/Resources/DWARF/a.out to github

--- circle_1/_get_next_line/get_next_line.c
+++ circle_1/_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By:  mchenava < mchenava@student.42lyon.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:24:16 by  mchenava         #+#    #+#             */
-/*   Updated: 2022/11/24 14:33:56 by  mchenava        ###   ########.fr       */
+/*   Updated: 2022/11/25 14:25:39 by  mchenava        ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,13 @@ char	*read_file(int fd, char *buff)
 		if (!tmp_buff)
 			return (NULL);
 		read_bytes = read(fd, tmp_buff, BUFFER_SIZE);
-		buff = ft_strjoin(buff, tmp_buff);
 		if (read_bytes == -1)
 		{
 			free(tmp_buff);
-			// printf("Error reading file\n");
-			return (NULL);
+			return (ft_calloc(1, sizeof(char)));
 		}
+		buff = ft_strjoin(buff, tmp_buff);
 	}
-	if (!buff[0])
-	{
-		free(buff);
-		return (NULL);
-	}
-	// printf ("buff: |%s|\n\n", buff);
 	return (buff);
 }
 
@@ -48,19 +41,17 @@ char	*next_buff(char *buff)
 	char	*next_buff;
 
 	i = 0;
-	if (!buff)
-		return (NULL);
 	while (buff[i] && buff[i] != '\n')
 		i++;
+	if (!buff[i])
+	{
+		free(buff);
+		return (NULL);
+	}
 	next_buff = ft_substr(buff, i + 1, ft_strlen(buff));
 	free(buff);
 	if (!next_buff)
 		return (NULL);
-	if (!next_buff[0])
-	{
-		free(next_buff);
-		return (NULL);
-	}
 	return (next_buff);
 }
 
@@ -69,9 +60,9 @@ char	*new_line(char *buff)
 	int		i;
 	char	*line;
 
-	if (!buff)
-		return (NULL);
 	i = 0;
+	if (!buff[i])
+		return (NULL);
 	while (buff[i] != '\n' && buff[i])
 		i++;
 	line = ft_substr(buff, 0, i + 1);
@@ -85,56 +76,56 @@ char	*get_next_line(int fd)
 	static char	*s_buff;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 /*|| read(fd, 0, 0) < 0*/)
 		return (NULL);
-	// printf("fd : |%d|\n", fd);
 	s_buff = read_file(fd, s_buff);
-	// printf("s_buff: |%s|\n", s_buff);
 	line = new_line(s_buff);
 	s_buff = next_buff(s_buff);
+	if (!s_buff)
+		free(s_buff);
 	return (line);
 }
 
-int	main(void)
-{
-	int		fd;
-	char	*line;
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
 
-	// fd = open("test.txt", O_RDONLY);
-	fd = open("/home/codespace/francinette/tests/get_next_line/fsoares/read_error.txt", O_RDONLY);
-	line = get_next_line(fd);
-	printf("line1: |%s|\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("line2: |%s|\n", line);
-	free(line);
-	if (BUFFER_SIZE > 100)
-	{
-		char *temp;
-		do {
-			temp = get_next_line(fd);
-			printf("temp: |%s|\n", temp);
-			free(temp);
-		} while (temp != NULL);
-	}
-	line = get_next_line(fd);
-	printf("line3: |%s|\n", line);
-	free(line);
-	close(fd);
-	fd = open("/home/codespace/francinette/tests/get_next_line/fsoares/read_error.txt", O_RDONLY);
-	line = get_next_line(fd);
-	printf("line4: |%s|\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("line5: |%s|\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("line6: |%s|\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("line7: |%s|\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("line8: |%s|\n", line);
-	free(line);
-}
+// 	// fd = open("test.txt", O_RDONLY);
+// 	fd = open("/Users/mchenava/francinette/tests/get_next_line/fsoares/read_error.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	printf("line1: |%s|\n", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("line2: |%s|\n", line);
+// 	free(line);
+// 	if (BUFFER_SIZE > 100)
+// 	{
+// 		char *temp;
+// 		do {
+// 			temp = get_next_line(fd);
+// 			printf("temp: |%s|\n", temp);
+// 			free(temp);
+// 		} while (temp != NULL);
+// 	}
+// 	line = get_next_line(fd);
+// 	printf("line3: |%s|\n", line);
+// 	free(line);
+// 	close(fd);
+// 	fd = open("/Users/mchenava/francinette/tests/get_next_line/fsoares/read_error.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	printf("line4: |%s|\n", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("line5: |%s|\n", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("line6: |%s|\n", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("line7: |%s|\n", line);
+// 	free(line);
+// 	line = get_next_line(fd);
+// 	printf("line8: |%s|\n", line);
+// 	free(line);
+// }
