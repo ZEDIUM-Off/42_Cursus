7335fcedd7159601b68209bd2ec29c0396240262
Matisse <matisse.chenavas@gmail.com>
2023-02-13 11:28:56 +0100

    pull

--- circle_2/_fdf/src/mlx_use/hooks/hooks.c
+++ circle_2/_fdf/src/mlx_use/hooks/hooks.c
@@ -6,14 +6,14 @@
 /*   By:  mchenava < mchenava@student.42lyon.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 12:47:51 by  mchenava         #+#    #+#             */
-/*   Updated: 2023/02/06 16:21:54 by  mchenava        ###   ########.fr       */
+/*   Updated: 2023/02/06 20:36:43 by  mchenava        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../main.h"
 #include <env3D.h>
 
-int cam_controls(int key, t_mlx_env **env)
+/*int cam_controls(int key, t_mlx_env **env)
 {
 	if (key == XK_w)
 		(*env)->env3d->cam->pos = vec4_add((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->forward,(*env)->env3d->cam->move_speed));
@@ -34,6 +34,33 @@ int cam_controls(int key, t_mlx_env **env)
 	if (key == XK_Left)
 		up_cam_yaw((*env)->env3d->cam, -(*env)->env3d->cam->rot_speed);
 	if (key == XK_Right)
+		up_cam_yaw((*env)->env3d->cam, (*env)->env3d->cam->rot_speed);
+	update_img(*env, (*env)->cube);
+	return (0);
+}*/
+
+int cam_controls(int key, t_mlx_env **env)
+{
+	printf("key pressed : %d \n", key);
+	if (key == 13)
+		(*env)->env3d->cam->pos = vec4_add((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->forward,(*env)->env3d->cam->move_speed));
+	if (key == 1)
+		(*env)->env3d->cam->pos = vec4_sub((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->forward,(*env)->env3d->cam->move_speed));
+	if (key == 0)
+		(*env)->env3d->cam->pos = vec4_sub((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->right,(*env)->env3d->cam->move_speed));
+	if (key == 2)
+		(*env)->env3d->cam->pos = vec4_add((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->right,(*env)->env3d->cam->move_speed));
+	if (key == 12)
+		(*env)->env3d->cam->pos = vec4_sub((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->up,(*env)->env3d->cam->move_speed));
+	if (key == 14)
+		(*env)->env3d->cam->pos = vec4_add((*env)->env3d->cam->pos, vec4_scale((*env)->env3d->cam->up,(*env)->env3d->cam->move_speed));
+	if (key == 126)
+		up_cam_pitch((*env)->env3d->cam, (*env)->env3d->cam->rot_speed);
+	if (key == 125)
+		up_cam_pitch((*env)->env3d->cam, -(*env)->env3d->cam->rot_speed);
+	if (key == 123)
+		up_cam_yaw((*env)->env3d->cam, -(*env)->env3d->cam->rot_speed);
+	if (key == 124)
 		up_cam_yaw((*env)->env3d->cam, (*env)->env3d->cam->rot_speed);
 	update_img(*env, (*env)->cube);
 	return (0);
