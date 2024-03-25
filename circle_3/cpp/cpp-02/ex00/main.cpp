eeba5d9dd5826b082cdaf6ee91dc66a0864a22fe
Matisse Chenavas <mchenava@z4r10p1.42lyon.fr>
2024-03-25 10:38:34 +0100

    change main cpp-02/ex00

--- circle_3/cpp/cpp-02/ex00/main.cpp
+++ circle_3/cpp/cpp-02/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By:  mchenava < mchenava@student.42lyon.fr>    +#+  +:+       +#+        */
+/*   By: mchenava <mchenava@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:40:12 by  mchenava         #+#    #+#             */
-/*   Updated: 2023/12/07 16:40:20 by  mchenava        ###   ########.fr       */
+/*   Updated: 2024/03/25 10:37:24 by mchenava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main() {
     Fixed c;
     c = b;
 
+    a.setRawBits(10);
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
